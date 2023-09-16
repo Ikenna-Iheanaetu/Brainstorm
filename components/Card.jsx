b92cfc94aae592a402d59e6cc500859fb2e37836
Ikenna Iheanaetu <ikennadeveloper@gmail.com>
2023-09-16 22:28:59 +0100

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,8 +3,8 @@ import { BsArrowRightShort } from "react-icons/bs";
 
 export default function Card({ data }) {
   return (
-     <div className="card p-5 bg-primary  w-fit mb-5">
-        <div className="flex items-center justify-between">
+     <div className="card p-5 bg-primary mb-5">
+        <div className="flex flex-col-reverse gap-3 min-[340px]:flex-row min-[340px]:items-center justify-between">
           <div className="flex flex-col">
             <span className="font-bold">{data.author.username}</span>
             <span className="text-sm truncate">{data.author.email}</span>
@@ -19,7 +19,7 @@ export default function Card({ data }) {
         </div>
         <div className="flex flex-col gap-3 mt-2">
           <span className="text-2xl text-accent">{data.quizName}</span>
-          <span className="truncate w-[20rem]">{data.quizDesc}</span>
+          <span className="truncate">{data.quizDesc}</span>
         </div>
         <div className="flex items-center gap-4 justify-between mt-5">
           <span className="text-[12px]">Created on {data.createdAt}</span>
