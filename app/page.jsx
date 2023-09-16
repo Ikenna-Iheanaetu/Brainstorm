db3690206480b483944a7d4e9cf8dcf68c319002
Ikenna Iheanaetu <ikennadeveloper@gmail.com>
2023-09-16 20:47:30 +0100

    Fixed the build error

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,12 @@ import Feed from "@/components/Feed";
 export default function Home() {
   return (
     <>
-      {" "}
       <div className="items-left flex flex-col gap-3">
         <h1 className="text-primary text-4xl">Brain storm</h1>
         <h3 className="text-secondary text-sm leading-6 tracking-wide justify-center">
           Unleash the Power of Your Mind and Dive into a World of Limitless
           Learning, Excitement, and Intellectual Adventure - The Brainstorm Quiz
-          App That's More Than Just a Game; It's Your Passport to Mastering
+          App that is More Than Just a Game, Its Your Passport to Mastering
           Topics, Challenging Your Friends, and Achieving Quiz Nirvana, All in
           One Place!
         </h3>
