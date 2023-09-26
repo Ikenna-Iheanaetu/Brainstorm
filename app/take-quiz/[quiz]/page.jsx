07dc683ef67c38ea39fb2bffd65176a6be4a7fe4
Ikenna Iheanaetu <ikennadeveloper@gmail.com>
2023-09-26 14:47:45 +0100

    Update page.jsx

--- app/take-quiz/[quiz]/page.jsx
+++ app/take-quiz/[quiz]/page.jsx
@@ -3,30 +3,40 @@
 import React, { useEffect, useState } from "react";
 
 export default function Page({ params }) {
-
   const [quizData, setQuizData] = useState([]);
+  const [author, setAuthor] = useState([]);
 
   async function getQuiz() {
-      const response = await fetch(`/api/quiz/${params?.quiz}`).then((res) => res.json());
+    const response = await fetch(`/api/quiz/${params?.quiz}`).then((res) =>
+      res.json()
+    );
 
-      if(response){
-        setQuizData(response.data)
-      } else{
-        console.log("Unexpected data structure")
-      }
+    if (response) {
+      setQuizData(response.data);
+      setAuthor(response.data.author);
+    } else {
+      console.log("Unexpected data structure");
+    }
   }
 
   useEffect(() => {
-    getQuiz()
-  }, [])
+    getQuiz();
+  }, []);
 
+  return (
+    <div className='block self-start'>
+      <div className="text-primary flex flex-col gap-4 place-items-start">
+        <span className="text-4xl">{`Author:  ${author.username}`}</span>
+        <span className="text-2xl">{`Quiz name:  ${quizData.quizName}`}</span>
+        <span className="text-2xl">{`Quiz description:  ${quizData.quizDesc}`}</span>
+        <span className="text-sm">{`Quiz creation:  ${quizData.createdAt}`}</span>
 
+        <p className='text-md text-secondary'>Ready to challenge your intellect and have some fun? Begin this quiz now!!</p>
 
-return(
-  <div>
-    <div className='text-primary'>{params?.quiz} {quizData._id}</div>
-  </div>
-)
-
+        <button className='white_btn'>
+          Begin Quiz
+        </button>
+      </div>
+    </div>
+  );
 }
-  
